e9d1d140bfcc2bc496ab0f75616c6116a7bcd82c
Qyang666 <qingyangli312@gmail.com>
2022-10-09 03:33:01 -0500

    first commit

---
+++ USfire_14-20.py
@@ -0,0 +1,38 @@
+import json
+
+infile= open('US_fires_9_14-1.json', 'r')
+fire_data = json.load(infile)
+
+#extract longitude, latitude, and brightness data
+bright, longitude, latitude = [],[],[]
+for i in fire_data:
+      if i['brightness']>450:
+            longitude.append(i['longitude'])
+            latitude.append(i['latitude'])
+            bright.append(i['brightness'])
+
+#check if data is loaded into lists
+print(longitude[:6])
+print(latitude[:6])
+print(bright[:6])
+
+
+from plotly.graph_objs import scattergeo, Layout
+from plotly import offline 
+
+data={'type':'scattergeo',
+      'lon':longitude,
+      'lat':latitude,
+      'marker':{
+            'size': 15,
+            'color': bright,
+            'colorscale': "Viridis",
+            'reversescale':True,
+            'colorbar':{'title':"Magnitude"}
+      }}
+
+mylayout = Layout(title='Fires in California')
+
+fig={"data": data, 'layout':mylayout}
+
+offline.plot(fig,filename='fires_in_california')
